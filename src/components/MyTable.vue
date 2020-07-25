7fb569c6caf0276ee1fce36a84cf9754185d20c9
yaojingxing <yaojingxing@yaojingxingdeMacBook-Pro.local>
2020-07-24 19:12:58 -0500

    2020 7.24

---
+++ src/components/MyTable.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="mytable">
+        <tr >
+            <th v-for="(item,key) in tabledata[0]" v-if="key!=='editable'">{{key}}</th>
+
+        </tr>
+        <tr v-for="(item,index) in tabledata">
+            <td v-for="(value,key) in tabledata[index]" v-if="key!=='editable'">
+                <span v-if="!isActive(index)">{{value}}</span>
+                <input v-else type="text" :placeholder=value v-model="tabledata[index][key]" >
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "table",
+        props: {
+            tabledata: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            }
+        },
+        methods: {
+            isActive(index) {
+                return this.user0[index].editable === true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
